64c39fae36b482dba22850fbad15c39198ae76b8
MinhoYiu <203593118+MinhoYiu@users.noreply.github.com>
2026-01-14 18:13:39 +0800

    Update 01_deploy_dltoken.ts

--- 前端/packages/hardhat/deploy/01_deploy_dltoken.ts
+++ 前端/packages/hardhat/deploy/01_deploy_dltoken.ts
@@ -3,11 +3,11 @@ import { DeployFunction } from "hardhat-deploy/types";
 import { Contract } from "ethers";
 
 /**
- * Deploys a contract named "DLToken" using the deployer account.
+ * Deploys a contract named "YMHToken" using the deployer account.
  *
  * @param hre HardhatRuntimeEnvironment object.
  */
-const deployDLToken: DeployFunction = async function (hre: HardhatRuntimeEnvironment) {
+const deployYMHToken: DeployFunction = async function (hre: HardhatRuntimeEnvironment) {
   /*
     On localhost, the deployer account is the one that comes with Hardhat, which is already funded.
 
@@ -17,11 +17,11 @@ const deployDLToken: DeployFunction = async function (hre: HardhatRuntimeEnviron
   const { deployer } = await hre.getNamedAccounts();
   const { deploy } = hre.deployments;
 
-  await deploy("DLToken", {
+  await deploy("YMHToken", {
     from: deployer,
     // Contract constructor arguments
-    // 注意：这里假设 DLToken 的构造函数没有参数。
-    // 如果你的 DLToken.sol constructor 需要参数 (e.g. constructor(string memory name, string memory symbol))
+    // 注意：这里假设 YMHToken 的构造函数没有参数。
+    // 如果你的 YMHToken.sol constructor 需要参数 (e.g. constructor(string memory name, string memory symbol))
     // 你需要在这里填充它们，例如: args: ["My DogeCoin", "MDG"],
     args: [],
     log: true,
@@ -31,12 +31,12 @@ const deployDLToken: DeployFunction = async function (hre: HardhatRuntimeEnviron
   });
 
   // Get the deployed contract to interact with it after deploying.
-  const dlToken = await hre.ethers.getContract<Contract>("DLToken", deployer);
-  console.log("DLToken deployed to:", await dlToken.getAddress());
+  const YMHToken = await hre.ethers.getContract<Contract>("YMHToken", deployer);
+  console.log("YMHToken deployed to:", await YMHToken.getAddress());
 };
 
-export default deployDLToken;
+export default deployYMHToken;
 
 // Tags are useful if you have multiple deploy files and only want to run one of them.
-// e.g. yarn deploy --tags DLToken
-deployDLToken.tags = ["DLToken"];
+// e.g. yarn deploy --tags YMHToken
+deployYMHToken.tags = ["YMHToken"];
